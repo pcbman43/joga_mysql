07b1768efd42f96a6a79fde437a1cd32293e7783
MKougia <kougia.mihkel@khk.ee>
2022-02-10 12:08:08 +0200

    Created project basic files

---
+++ index.js
@@ -0,0 +1,23 @@
+// application packages
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+const mysql = require('mysql')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+// create database connection
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "qwerty",
+	database: "joga_mysql"
+})
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log('App is started at http://localhost:3000')
+})
